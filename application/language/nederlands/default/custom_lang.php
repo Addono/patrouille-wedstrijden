acfd189d30122404e8b5535b8971cef387ce7df0
Addono <a.d.knapen@student.tue.nl>
2017-03-14 23:11:28 +0100

    refactor: more translation fixes

--- application/language/nederlands/default/custom_lang.php
+++ application/language/nederlands/default/custom_lang.php
@@ -5,7 +5,7 @@
  */
 
 // @Overwrite
-$lang['application_name'] = 'Patrioulle wedstrijden 2017';
-$lang['application_title'] = 'Patrioulle wedstrijden 2017';
+$lang['application_name'] = 'Patrouille wedstrijden 2017';
+$lang['application_title'] = 'Patrouille wedstrijden 2017';
 $lang['copyright'] = 'Scouting Impeesa';
 $lang['application_version'] = '';
--- application/views/page/default/default-header.php
+++ application/views/page/default/default-header.php
@@ -5,7 +5,7 @@
  */
 ?>
 <div class="col-md-12 col-md-offset-0 vmargin">
-    <h1>Patrioulle wedstrijden 2017</h1>
+    <h1><?=lang('application_name')?></h1>
     <h4>Hier vind je hoe de punten verdeeld worden en de huidige tussenstand.</h4>
     <a class="btn btn-warning" href="<?=site_url('Week1')?>">Week 1</a>
 </div>
